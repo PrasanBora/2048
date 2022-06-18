6f4b8396e73620f3d23fac0436fbd548f0626a76
PrasanBora <prasanbora23@gmail.com>
2022-06-18 23:49:57 +0530

    moveup function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const gameBoard =document.querySelector(".gameBoard")
-const cell =document.querySelectorAll(".box");
+// const cell =document.querySelectorAll(".box");
 const userScore= document.querySelector(".points")
 // cell[0].innerHTML=34;
 
@@ -8,7 +8,20 @@ let score =0;
 let filledCell=[[0 , 0 , 0 , 0] , [0 , 0 , 0 , 0]
 ,[0 , 0 , 0 , 0],[0 , 0 , 0 , 0]]; //array to mark cell with value 
 
+   for(let j=0;j<4;j++)
+    {
+        for(let k=0;k<4;k++)
+        {
+            let  cell=document.createElement("div");
+            cell.id=`index${j}-${k}`;
+            gameBoard.append(cell);
+            
+            updateValue(j,k);
 
+           
+        }
+    }
+    // updateValue();
 
 // function to genrate a new cell value i.e 2 or 4
 generate =()=>{
@@ -23,13 +36,14 @@ generate =()=>{
 
 // function to get a new position for new cell 
 newPosition=()=>{
-    let newValue =Math.floor(Math.random()*16);
+    let newi =Math.floor(Math.random()*4);
+    let newj =Math.floor(Math.random()*4);
     
-    if(filledCell[newValue]=== 1)
+    if(filledCell[newi][newj]!=0)
     newPosition();
     else 
-    cell[newValue].innerHTML= generate();
-    filledCell[newValue]=1;
+    filledCell[newi][newj]=generate();
+    updateValue(newi,newj);
 }
 newPosition();
 
@@ -38,13 +52,15 @@ document.querySelector("body").addEventListener('keyup',(e)=>{
     switch(e.key)
     {
             case "ArrowUp":
-                newPosition();
+                
                 moveUp();
+                newPosition();
               break
 
             case "ArrowDown":
-                newPosition();
+                
                 moveDown();
+                newPosition();
                 break
 
             case "ArrowRight":
@@ -57,70 +73,50 @@ document.querySelector("body").addEventListener('keyup',(e)=>{
     }
 });
 
-// console.log(filledCell[11]);
 
-// function merge( to , from )
-// {
-//    if(cell[to].innerHTML===cell[from].innerHTML)
-//    {
-//     cell[from].innerHTML=2*cell[from].innerHTML;
-//     score=score+Number(cell[from].innerHTML);
-//     userScore.innerHTML=score;
-//     cell[to].innerHTML="";
-//     filledCell[to]=0;
-//     return;
-//    }
-//    else 
-//    {
-//     return;
-//    }
-// }
+function updateValue(i,j)
+{
+    
+            if(filledCell[i][j]>0)
+            document.querySelector(`#index${i}-${j}`).innerHTML=filledCell[i][j];
+             
+            else
+            {
+                document.querySelector(`#index${i}-${j}`).innerHTML="";  
+            }
+            
 
-// function moveUp ()
-// {
-//     for(let i=4 ; i<16;i++)
-//      {
-//         // console.log(filledCell[i] );
+        }
+ 
+function moveUp()
+{
+    for(let j=0;j<4;j++)
+    {
+        console.log("--     555555555--");
+        for(let i=3;i>0;i--)
+        {
+            console.log(filledCell[i][j]);
+            console.log("----");
+            if(filledCell[i][j]===filledCell[i-1][j] && filledCell[i][j]!=0)
+            {  
+                // merge
+                filledCell[i-1][j]=2*filledCell[i][j];
+                filledCell[i][j]=0;
+                score=score+filledCell[i-1][j];
+                userScore.innerHTML=score;
+                updateValue(i,j);
+                updateValue(i-1,j);
+            }
 
-        
-//         if(filledCell[i]===1 && filledCell[i-4]===1)
-//         {  
-//             merge(i-4,i);
-//         }
-//         if(filledCell[i]===1 && filledCell[i-4]===0)
-//         {
-//             cell[i-4].innerHTML=cell[i].innerHTML;
-//             cell[i].innerHTML="";
-//             filledCell[i-4]=1;
-//             filledCell[i]=0;
+             else if(filledCell[i][j] && !filledCell[i-1][j])
+            {
+                filledCell[i-1][j] =filledCell[i][j];
+                filledCell[i][j]=0;
+                updateValue(i,j);
+                updateValue(i-1,j);
+            }
 
-//             // console.log(filledCell[i] , filledCell[i-4],cell[i].innerHTML,cell[i-4].innerHTML );
+        }
+    }
+}
 
-//         }
-
-//       }  
-// }
-
-// function moveDown ()
-// {
-//     for(let i=0 ; i<12;i++)
-//      {
-//         console.log(filledCell[i] );
-
-//         if(filledCell[i]===1 && filledCell[i+4]===1)
-//         {  
-//             merge(i,i+4);
-//         }
-//         if(filledCell[i]===1 && filledCell[i+4]===0)
-//         {
-//             cell[i+4].innerHTML=cell[i].innerHTML;
-//             cell[i].innerHTML="";
-//             filledCell[i+4]=1;
-//             filledCell[i]=0;
-
-//             // console.log(filledCell[i] , filledCell[i-4],cell[i].innerHTML,cell[i-4].innerHTML );
-
-//         }
-
-//       }  
-// }
